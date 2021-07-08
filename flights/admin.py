c5d40eba75b0a3881b209b9afd599312476e2657
jomapormentilla <jomapormentilla@gmail.com>
2021-07-08 14:42:42 -0400

    Add form to add a passenger to a flight

--- flights/admin.py
+++ flights/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 
 # Register your models here.
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights")
+
 admin.site.register(Airport)
 admin.site.register(Flight)
 admin.site.register(Passenger)
--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,8 @@
-from django.http.response import HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import *
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -14,14 +14,13 @@ def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.object.get(pk=int(request.POST["passenger"]))
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
-
-    return render(request)
+        return HttpResponseRedirect(reverse("flight", args=[flight_id]))
