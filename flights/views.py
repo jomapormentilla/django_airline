866989c3ec73409863ad7d7c56388d011b1769d7
jomapormentilla <jomapormentilla@gmail.com>
2021-07-08 14:12:55 -0400

    Add '/book' route. Define 'book' method in controller.

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Flight
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,12 @@ def flight(request, flight_id):
         "flight": flight,
         "passengers": flight.passengers.all()
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.object.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+
+    return render(request)
